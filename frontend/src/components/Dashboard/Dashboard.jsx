8037b7ad2f815a06bdacc1b746e3373fff04a809
Sachin Koli <sachinkoli8477@gmail.com>
2026-06-30 22:54:54 +0530

    added the heading on main container

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import './Dashboard.css';
+import React from "react";
+import "./Dashboard.css";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dashboard-container">
+      <div className="dashboard-wrapper">
+        <div className="main-container">
+          <div className="">
+          <h1 className="heading">Smart Resume Screening</h1>
+          <h2 className="sub-heading">Resume Match Score</h2>
+          </div>
+        </div>
+        <div className="profile-container">second</div>
+      </div>
+    </div>
+  );
 }
