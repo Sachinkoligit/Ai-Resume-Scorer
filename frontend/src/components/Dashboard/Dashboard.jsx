d57dc717c29e6f0144804b4b71d8c0754c00ed3f
Sachin Koli <sachinkoli8477@gmail.com>
2026-07-01 23:22:03 +0530

    complete the dashboard design

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dashboard.css";
 import { MdOutlineIntegrationInstructions } from "react-icons/md";
-import { FaRegFileAlt } from "react-icons/fa";
+import { FaRegFileAlt, FaUser } from "react-icons/fa";
 import { VscFilePdf } from "react-icons/vsc";
+import { GiProgression } from "react-icons/gi";
 
 export default function Dashboard() {
+  const [imageError, setImageError] = useState(false);
   return (
     <div className="dashboard-container">
       <div className="dashboard-wrapper">
@@ -21,7 +23,7 @@ export default function Dashboard() {
                 Important Instructions:
               </p>
               <li>
-                <FaRegFileAlt color="blue"/>
+                <FaRegFileAlt color="blue" />
                 <span>
                   Please paste the complete job description in the "Job
                   Description" field before submitting.
@@ -36,20 +38,43 @@ export default function Dashboard() {
 
           <div className="upload-wrapper">
             <div className="container">
-              <h4 style={{margin:0}}>Upload your resume</h4>
+              <h4 style={{ margin: 0 }}>Upload your resume</h4>
             </div>
 
-            <button className="upload-button">
-              Upload Resume
-            </button>
+            <button className="upload-button">Upload Resume</button>
           </div>
 
           <div className="analyze-wrapper">
-            <textarea rows={8} placeholder="Paste Your Job Description"/>
+            <textarea rows={8} placeholder="Paste Your Job Description" />
             <button className="analyze-button">Analyze</button>
           </div>
         </div>
-        <div className="profile-container">second</div>
+
+        <div className="right-container">
+        <div className="profile-container">
+          <h3>Analyze with AI</h3>
+          <div className="profile-image-container">
+            {!imageError ? (
+              <img
+                alt="profile"
+                src="xyz.png"
+                onError={() => setImageError(true)}
+                className="profile"
+              />
+            ) : (
+              <FaUser className="profile"/>
+            )}
+          </div>
+          <h4>Sachin koli</h4>
+        </div>
+
+        <div className="profile-container">
+          <h3>Result</h3>
+          <h2>75% {" "} <GiProgression color="orange"/></h2> 
+          <h5>Feedback</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste architecto quo aperiam ad porro recusandae, enim veniam debitis, perferendis omnis velit dolores consequuntur temporibus maxime deserunt, reiciendis ullam nihil dicta laboriosam sint. Maxime explicabo quis nisi eaque, aperiam velit magnam perferendis asperiores iste beatae reiciendis quaerat corrupti autem, necessitatibus molestias? eaque, aperiam velit magnam perferendis asperiores iste beatae reiciendis quaerat corrupti autem, necessitatibus molestias?</p>
+        </div>
+        </div>
       </div>
     </div>
   );
