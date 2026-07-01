638af599a4658175268e6f0a4a8372a3394dcd71
Sachin Koli <sachinkoli8477@gmail.com>
2026-07-01 08:12:14 +0530

    created the dashboard main page

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Dashboard.css";
+import { MdOutlineIntegrationInstructions } from "react-icons/md";
+import { FaRegFileAlt } from "react-icons/fa";
+import { VscFilePdf } from "react-icons/vsc";
 
 export default function Dashboard() {
   return (
@@ -7,8 +10,43 @@ export default function Dashboard() {
       <div className="dashboard-wrapper">
         <div className="main-container">
           <div className="">
-          <h1 className="heading">Smart Resume Screening</h1>
-          <h2 className="sub-heading">Resume Match Score</h2>
+            <h1 className="heading">Smart Resume Screening</h1>
+            <h2 className="sub-heading">Resume Match Score</h2>
+          </div>
+
+          <div className="instruction-container">
+            <ul className="instructions-list">
+              <p className="">
+                <MdOutlineIntegrationInstructions color="blue" />
+                Important Instructions:
+              </p>
+              <li>
+                <FaRegFileAlt color="blue"/>
+                <span>
+                  Please paste the complete job description in the "Job
+                  Description" field before submitting.
+                </span>
+              </li>
+              <li>
+                <VscFilePdf color="blue" />{" "}
+                <span>Only PDF format (.pdf) resumes are accepted.</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="upload-wrapper">
+            <div className="container">
+              <h4 style={{margin:0}}>Upload your resume</h4>
+            </div>
+
+            <button className="upload-button">
+              Upload Resume
+            </button>
+          </div>
+
+          <div className="analyze-wrapper">
+            <textarea rows={8} placeholder="Paste Your Job Description"/>
+            <button className="analyze-button">Analyze</button>
           </div>
         </div>
         <div className="profile-container">second</div>
