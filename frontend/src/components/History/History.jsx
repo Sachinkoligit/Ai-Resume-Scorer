b8e5412e095c49bcd27ba54455d763a6a90d6bc0
Sachin Koli <sachinkoli8477@gmail.com>
2026-07-02 08:15:53 +0530

    updates the history page

--- frontend/src/components/History/History.jsx
+++ frontend/src/components/History/History.jsx
@@ -11,19 +11,7 @@ export default function History() {
         <Result/>
         <Result/>
         <Result/>
-        <div className="profile-container">
-              <h3>Result</h3>
-              <h2>
-                75% <GiProgression color="orange" />
-              </h2>
-              <h5>Feedback</h5>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste architecto
-                quo aperiam ad porro recusandae, enim veniam debitis, perferendis omnis
-                velit dolores consequuntur temporibus maxime deserunt, reiciendis ullam
-                nihil dicta laboriosam sint
-              </p>
-            </div>
+        <Result/>
       </div>
     </div>
   )
