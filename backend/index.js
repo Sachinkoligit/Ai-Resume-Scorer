29d19df15fda0fe8278a15aad7e00d6f6dca4154
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-10 23:23:28 +0530

    conneting with database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+import { Connection } from "./connection.js";
 
 const app = express();
 const PORT=5000;
+configDotenv();
+
+Connection();
 
 app.get("/",(req,res)=>{
     res.send("Hello from Backend!");
