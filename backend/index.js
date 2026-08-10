bed53fe1334d8f083152f816c90039602befe27f
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-10 22:44:58 +0530

    Create first get request from backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
-const PORT = 5000;
+const PORT=5000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello from Backend!");
+})
 
 app.listen(PORT, ()=>{
-    console.log("Server is running on ",PORT)
+    console.log("Server is running on",PORT)
 })
