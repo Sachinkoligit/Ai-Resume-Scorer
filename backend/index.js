dffa9988710c1a0622ea8af9757cf606f87d8e15
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-11 23:47:56 +0530

    Successfully connected the database with the backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
 import express from "express";
-import { configDotenv } from "dotenv";
+import dotenv from "dotenv";
+import dns from "dns";
 import { Connection } from "./connection.js";
 
+dotenv.config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const app = express();
-const PORT=5000;
-configDotenv();
+
+const PORT = process.env.PORT || 5000;
 
 Connection();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello from Backend!");
-})
+});
 
-app.listen(PORT, ()=>{
-    console.log("Server is running on",PORT)
-})
+app.listen(PORT, () => {
+    console.log("Server is running on", PORT);
+});
