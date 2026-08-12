aa323f6426cf59a67991d46eea24b7f3e529f1f7
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-12 23:30:13 +0530

    Created the user model

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    role:{
+        type: String,
+        default:"user"
+    },
+    photourl:{
+        type:String
+    }
+},
+{timestamps:true});
+
+const userModal = mongoose.model("user",userSchema);
