40bdd1f28b8a261a933fef951a1feefb518ab535
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-13 23:40:46 +0530

    Created the controller for register user.

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import dns from "dns";
 import { Connection } from "./connection.js";
+import authRoutes from "./routes/user.route.js"
 
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 Connection();
 
-app.get("/", (req, res) => {
-    res.send("Hello from Backend!");
-});
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on", PORT);
