04148a102dd4261b9c53ed66433f294e491cbecf
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-17 23:22:07 +0530

    resume controller now returns the data inside the array

--- backend/controllers/resume.controller.js
+++ backend/controllers/resume.controller.js
@@ -3,13 +3,20 @@ import { PDFParse } from "pdf-parse";
 import { Cohere } from "cohere-ai";
 import upload from "../config/multer.js";
 
-export const storeResume = async(req,res) =>{
-    try{
-        const {job_desc, user} = req.body;
-        console.log("job description",job_desc)
-        res.status(200).json({message:"Success",data:[job_desc,user,req.file]})
-    } catch(error){
-        console.log("error",error)
-        res.status(500).json({message:"Internal server error",error:error.message});
-    }
-}
+export const storeResume = async (req, res) => {
+  try {
+    const { job_desc, user } = req.body;
+    console.log("job description", job_desc);
+    res
+      .status(200)
+      .json({
+        message: "Success",
+        data: [{ job_desc: job_desc }, { user: user }, { resume: req.file }],
+      });
+  } catch (error) {
+    console.log("error", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
