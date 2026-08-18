303b56161a2ac4f0cfc91cb6cda639519055234e
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-18 23:12:36 +0530

    created the prompt for the ai

--- backend/controllers/resume.controller.js
+++ backend/controllers/resume.controller.js
@@ -2,21 +2,27 @@ import resumeModal from "../models/resume.js";
 import { PDFParse } from "pdf-parse";
 import { Cohere } from "cohere-ai";
 import upload from "../config/multer.js";
-import fs from 'fs'
+import fs from "fs";
 
 export const storeResume = async (req, res) => {
   try {
     const { job_desc, user } = req.body;
     const pdfBuffer = fs.readFileSync(req.file.path);
-    const parser = new PDFParse({data: pdfBuffer});
+    const parser = new PDFParse({ data: pdfBuffer });
     const result = await parser.getText();
     console.log(result);
-    res
-      .status(200)
-      .json({
-        message: "Success",
-        data: [{ job_desc: job_desc }, { user: user }, { resume: req.file }],
-      });
+
+    const prompt = `
+    You are a resume screening assistant. Compare the following resume text with the provided job description and give a match score (0-100) and feedback.
+
+    Resume: ${result.text}
+    Job Description: ${job_desc}
+    `;
+
+    res.status(200).json({
+      message: "Success",
+      data: [{ job_desc: job_desc }, { user: user }, { resume: req.file }],
+    });
   } catch (error) {
     console.log("error", error);
     res
