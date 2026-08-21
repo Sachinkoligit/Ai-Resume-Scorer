7cc3bf2a79db37f141a7628ee6a43b5a3a471671
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-21 23:15:36 +0530

    Fixed the gemini api integration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import dns from "dns";
 import { Connection } from "./connection.js";
 import authRoutes from "./routes/user.route.js"
 import resumeRoutes from "./routes/resume.route.js"
 
-dotenv.config();
+// dotenv.config();
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
