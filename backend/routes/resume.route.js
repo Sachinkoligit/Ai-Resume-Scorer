1a051b6379179caa0b7551fb9fe9ddb2b21a0859
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-25 20:52:41 +0530

    Add getAllResumeForUser endpoint and clean up resume file after scoring

--- backend/routes/resume.route.js
+++ backend/routes/resume.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { storeResume } from "../controllers/resume.controller.js";
+import { getAllResumeForUser, storeResume } from "../controllers/resume.controller.js";
 import upload from "../config/multer.js";
 
 const router = express.Router();
 
 router.post("/addResume",upload.single("resume"),storeResume);
+
+router.post("/getUserResume",getAllResumeForUser);
 
 export default router;
