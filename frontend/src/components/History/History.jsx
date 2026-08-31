9bb51f817251eed574250772f3f44a290aa1dfe1
Sachin Koli <sachinkoli8477@gmail.com>
2026-08-31 23:40:04 +0530

    Refactor History component to fetch and display user resume history

--- frontend/src/components/History/History.jsx
+++ frontend/src/components/History/History.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import './History.css';
-import Result from '../ui/Result';
+import React, { useEffect, useState } from "react";
+import "./History.css";
+import Result from "../ui/Result";
 // import "../Dashboard/Dashboard.css";
 import { GiProgression } from "react-icons/gi";
+import { useAuthStore } from "../../store/authStore";
 
 export default function History() {
+  const { authUser } = useAuthStore();
+  const [resumes, setResumes] = useState([]);
+  const getUserHistory = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/auth/getUserResume/${authUser._id}`,
+      );
+      const result = await response.json();
+      setResumes(result.data);
+    } catch (error) {
+      console.log(error || error.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserHistory();
+  }, []);
   return (
-    <div className='history-container'>
-      <div className='history-wrapper'>
-        <Result/>
-        <Result/>
-        <Result/>
-        <Result/>
+    <div className="history-container">
+      <div className="history-wrapper">
+        {/* <Result />
+        <Result />
+        <Result />
+        <Result /> */}
+        {resumes.map((item)=>(
+          <Result score={item.score} feedback={item.feedback} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
