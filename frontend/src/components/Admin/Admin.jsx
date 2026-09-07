f6898e2c7ec3f6e9aa5f07a53fe8702b1e559f67
Sachin Koli <sachinkoli8477@gmail.com>
2026-09-07 23:11:54 +0530

    Implement user history fetching in Admin component and enhance AdminResults for dynamic data display

--- frontend/src/components/Admin/Admin.jsx
+++ frontend/src/components/Admin/Admin.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Admin.css";
 import AdminResults from "../ui/AdminResults";
+import { useAuthStore } from "../../store/authStore";
 
 export default function Admin() {
+  const { authUser } = useAuthStore();
+  const [resumes, setResumes] = useState([]);
+
+  const getUserHistory = async () => {
+    if (!authUser?._id) {
+      setResumes([]);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/auth/getAllResume`,
+      );
+      const result = await response.json();
+      setResumes(result.data || []);
+    } catch (error) {
+      console.log(error || error.message);
+      setResumes([]);
+    }
+  };
+
+  useEffect(() => {
+    getUserHistory();
+  }, [authUser?._id]);
+
+  if (resumes.length < 1) {
+    return (
+      <div className="history-container">
+        <h2>No History Found</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="admin-container">
       <div className="admin-wrapper">
-        <AdminResults/>
-        <AdminResults/>
-        <AdminResults/>
-        <AdminResults/>
+        {resumes.map((item, index) => (
+          <AdminResults
+            key={index}
+            name={authUser.name}
+            email={authUser.email}
+            score={item.score}
+            feedback={item.feedback}
+            issueDate={item.createdAt}
+          />
+        ))}
       </div>
     </div>
   );
